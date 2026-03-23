661b4f7ba475ae1367092250537e15fb76edf0a1
Jesse Sher <jebba272727@gmail.com>
2026-03-24 08:01:14 +1100

    added the entity adding feature, (entities arent loading will fix)

--- src/MainScene/MainScene.cpp
+++ src/MainScene/MainScene.cpp
@@ -1,8 +1,19 @@
 #include "MainScene.hpp"
 
+#include <emscripten/bind.h>
+
 void Main::Init()
 {
     background_color = SKYBLUE;
+}
+void CreateEntity()
+{
+    std::unique_ptr<Entity> new_entity = std::make_unique<Entity>();
+    new_entity->texture = LoadTexture("assets/cat.png");
+    new_entity->x = GetRandomValue(0, game.WIDTH);
+    new_entity->y = GetRandomValue(0, game.HEIGHT);
+
+    game.AddEntity(std::move(new_entity));
 }
 
 void Main::Update()
@@ -20,5 +31,7 @@ void Main::DrawUI()
 
 }
 
-EMSCRIPTEN_BINDINGS(main_module) {
+EMSCRIPTEN_BINDINGS(main_module)
+{
+    emscripten::function("create_entity", &CreateEntity);
 }
--- wbuild.bat
+++ wbuild.bat
@@ -5,4 +5,4 @@ set CODE_FILES=
 for /R src %%f in (*.cpp) do (
     set "CODE_FILES=!CODE_FILES! %%f"
 )
-em++ -o index.js %CODE_FILES% -std=c++17 -Os -Wall ./lib/libraylib.web.a -I. -I include/ -L. -L lib/ -lembind -s USE_GLFW=3 -DPLATFORM_WEB
+em++ -o index.js %CODE_FILES% -std=c++17 -Os -Wall ./lib/libraylib.web.a -I. -I include/ -L. -L lib/ -lembind -s USE_GLFW=3 --preload-file assets/ -DPLATFORM_WEB
